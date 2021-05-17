0cc81399e06e6468076d2bf819375517d2767504
Bradley <setokibah@gmail.com>
2021-05-17 02:26:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# Purpose of program will be to take values of our raids that we give it, 
+# and then give us a total sum at the end of what was gained or lost.
+# Will iron out what specific things we need to track to achieve this.
+
+
+# What do we want?
+#   1- Cost of gearing up
+#   2- What was gained as a result of raid
+#   3- Cost of re-arming/repairing tools
+
+# Input needed
+# Pre-Raid: 
+#   Gear cost(if any)
+#   Map heading to
+
+def pre_raid():
+    pass
+
+# Post-Raid:
+#   Total from sold goods
+#   Re-arm costs
+#       Ammo purchases
+#       Armor repair
+#       Consumable restock
+
+def post_raid():
+    pass
+    # return total_sum
+
+# End of program save session positive or negative to a file?
+def export_results():
+    pass
+
+def main():
+    pass
+    #while True:
+        #pre_raid()
+        #post_raid_gain = post_raid()
+        #
+        # response = input("Another raid? (y/n): ")
+        # if response == y:
+        #       break
+        # else:
+        #       continue
+main()
