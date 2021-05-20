43bb7dbd4dbbbe7316df17c1c15edfa3af401abb
Bradley <setokibah@gmail.com>
2021-05-20 00:43:35 +0000

    First implementation of tracking idea

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
-# Purpose of program will be to take values of our raids that we give it, 
-# and then give us a total sum at the end of what was gained or lost.
-# Will iron out what specific things we need to track to achieve this.
-
-
-# What do we want?
-#   1- Cost of gearing up
-#   2- What was gained as a result of raid
-#   3- Cost of re-arming/repairing tools
-
-# Input needed
-# Pre-Raid: 
-#   Gear cost(if any)
-#   Map heading to
-
 def pre_raid():
     pass
-
-# Post-Raid:
-#   Total from sold goods
-#   Re-arm costs
-#       Ammo purchases
-#       Armor repair
-#       Consumable restock
-
+    gear_cost = int(input('Enter initial gear cost: '))
+    map = input('Enter map raiding on: ')
+    return gear_cost, map
+    
 def post_raid():
-    pass
-    # return total_sum
+    profit = int(input('Total Rouble Gain: '))
+    cost = int(input('Enter total re-arming cost: '))
+    total = (profit - cost)
+    return int(profit - cost)
 
 # End of program save session positive or negative to a file?
 def export_results():
     pass
 
 def main():
-    pass
-    #while True:
-        #pre_raid()
-        #post_raid_gain = post_raid()
-        #
-        # response = input("Another raid? (y/n): ")
-        # if response == y:
-        #       break
-        # else:
-        #       continue
+    list_of_gains = []
+    map_list = []
+    end_of_session_gains = 0
+    maps_ran = 0
+
+    while True:
+        gear_cost, map = pre_raid()
+        post_raid_gain = post_raid()
+        map_list.append(map)
+        end_gain = post_raid_gain - gear_cost
+        end_of_session_gains += end_gain
+        maps_ran += 1
+
+        list_of_gains.append(end_gain)
+
+        print('\n\nGains for the session so far: ')
+        for gain, current_map in zip(list_of_gains, map_list):
+            print(f'₽{gain} on {current_map}.')
+
+        response = input("\nAnother raid? (y/n): ")
+        if response == 'y':
+            continue
+        elif response == 'n':
+            print(f'----------------------------------------------\nTotal gains for the session: ₽{end_of_session_gains}.\nTotal Maps Raided: {maps_ran}.\n----------------------------------------------')
+            break
+        else:
+            print('You did not give a valid response, program will end.')
+            break
 main()
